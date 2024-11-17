e58767d2460a4726f47eeaec91a87c06afdc3239
Asad <realasadhussain@gmail.com>
2024-11-17 16:48:50 +0500

    tagline changed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Meow Meow Nigga
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React try 84r2398fffghj
+          Learn React
         </a>
       </header>
     </div>
